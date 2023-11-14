c90e837af0592ab3c1f454c5e4dfd89637d44143
AliciaXia222 <143651934+AliciaXia222@users.noreply.github.com>
2023-11-14 02:19:53 +0000

    update split spam in split file

--- Split.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-import numpy as np
-completeSpam = pd.read_csv("completeSpamAssassin.csv")
-# 删除空行
-df = pd.DataFrame(completeSpam)
-df.dropna(subset=['Body'], inplace=True)
-# 拆分每个邮件为单词列表
-df['words'] = df['Body'].apply(lambda email: 
- [word for line in email.split('\n') for word in line.split()])
